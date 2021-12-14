3aa68c81bbee1d1496a4e8403579d926110c1f08
雾山 <li997477295@outlook.com>
2021-12-14 10:03:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,95 +1,52 @@
-/** Telegram机器人的Token */
-const token = '机器人的Token'
+/** Telegram机器人Token */
+const token = '机器人Token'
 const robotName = '@寻龙'
 
 const TelegramBot = require('node-telegram-bot-api')
 const cheerio = require('cheerio')
 const axios = require('axios')
-const fs = require('fs')
 const moment = require('moment')
 moment.locale('zh-cn')
 const vm = require('vm')
-const siteUrl = 'https://www.javbus.com'
+const javUrl = 'https://www.javbus.com'
 const embedyUrl = 'https://embedy.cc'
 const xvideoUrl = 'https://www.xvideos.com'
-const bestUrl = 'https://xhamster.com'
-// const videoUrl = 'https://watchjavonline.com'
+const xhamsterUrl = 'https://xhamster.com'
 
-const http = axios.create({
-  baseURL: siteUrl,
-  timeout: 15000,
-  headers: {
-    'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
-    Accept:
-      'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.9'
-  }
-})
+const httpGet = config => {
+  return new Promise((resolve, reject) => {
+    const instance = axios.create({
+      method: 'get',
+      timeout: 5000,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language': 'zh-CN,zh;q=0.9'
+      }
+    })
 
-// const httpV = axios.create({
-//   baseURL: videoUrl,
-//   timeout: 15000,
-//   headers: {
-//     'User-Agent':
-//       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
-//     Accept:
-//       'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-//     'Accept-Language': 'zh-CN,zh;q=0.9'
-//   }
-// })
-
-const httpE = axios.create({
-  baseURL: embedyUrl,
-  timeout: 15000,
-  headers: {
-    'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
-    Accept:
-      'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.9'
-  }
-})
-
-const httpX = axios.create({
-  baseURL: xvideoUrl,
-  timeout: 15000,
-  headers: {
-    'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
-    Accept:
-      'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.9'
-  }
-})
-
-const httpB = axios.create({
-  baseURL: bestUrl,
-  timeout: 15000,
-  headers: {
-    'User-Agent':
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.117 Safari/537.36',
-    Accept:
-      'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Language': 'zh-CN,zh;q=0.9'
-  }
-})
+    instance(config).then(res => {
+      resolve(res);
+    }).catch(err => {
+      reject(err);
+    })
+  })
+}
 
 const bot = new TelegramBot(token, { polling: true })
 
 const helpMsg = `
-  欢迎使用番号机器人\n
+  欢迎使用寻龙机器人 \n
   请输入命令格式: \n
     /av ssni-888 查询 \n
     /xv 麻豆 查询 \n
     /xm hot 查询 \n
+  由 Cloudflare Worker 强力驱动
 `
 
 bot.onText(/\/start/, msg => {
-  bot.sendMessage(msg.chat.id, helpMsg)
-})
-
-bot.onText(/\/av/, msg => {
   bot.sendMessage(msg.chat.id, helpMsg)
 })
 
@@ -109,29 +66,11 @@ bot.onText(/\/state (\d+)/, (msg, match) => {
   return bot.sendMessage(msg.chat.id, buffer)
 })
 
-function drawState(range) {
-  let now = moment()
-  let earlyDay = moment().subtract(range, 'day')
-  let date = [],
-    data = []
-  while (earlyDay.diff(now) <= 0) {
-    let dateKey = earlyDay.format('YYYY-MM-DD')
-    date.push(dateKey)
-    if (state.date[dateKey]) data.push(state.date[dateKey])
-    else data.push(0)
-    earlyDay = earlyDay.add(1, 'day')
-  }
-  let message =
-    '从 ' +
-    moment(state.start).fromNow() +
-    ' 开始工作\n\n       日期       : 查询车牌号次数'
-  date.forEach((d, i) => {
-    message += '\n' + d + ' : ' + data[i]
-  })
-  return message
-}
+
 
 let idRegex = /^([a-z]+)(?:-|_|\s)?([0-9]+)$/
+
+///////////////// 指令 ///////////////////////////////
 
 bot.onText(/\/av (.+)/, async (msg, match) => {
   const today = moment().format('YYYY-MM-DD')
@@ -141,12 +80,12 @@ bot.onText(/\/av (.+)/, async (msg, match) => {
   let chartType = msg.chat.type
   let isPrivate = chartType === 'private'
   let id = match[1].trim()
-  console.log('请求番号', id)
+  console.log('开始请求', id)
   if (idRegex.test(id)) {
     id = id.match(idRegex)
     id = id[1] + '-' + id[2]
   }
-  if (isPrivate) bot.sendMessage(chatId, `开始查找车牌号：${id} ……`)
+  if (isPrivate) bot.sendMessage(chatId, `开始查找车牌：${id} ……`)
   try {
     let result = await parseHtml(id)
     if (result.title) {
@@ -196,98 +135,10 @@ bot.onText(/\/av (.+)/, async (msg, match) => {
   } catch (e) {
     console.error(id, e.message)
     if (e.message.indexOf('timeout') !== -1)
-      return bot.sendMessage(chatId, '机器人查询番号超时，请重试')
+      return bot.sendMessage(chatId, '机器人查询车牌超时，请重试')
     bot.sendMessage(chatId, `找不到 ${id}！`)
   }
 })
-
-async function parseHtml(id) {
-  const result = { title: '', cover: '', magnet: [], list: [] }
-  let response = await http.get('/' + id)
-  let $ = cheerio.load(response.data)
-  let $image = $('a.bigImage img')
-  result.cover = siteUrl + $image.attr('src')
-  result.title = $image.attr('title')
-  let ajax = { gid: '', uc: '', img: '' }
-  const context = new vm.createContext(ajax)
-  let $script = $('body > script:nth-child(9)')
-  new vm.Script($script.html()).runInContext(context)
-  let floor = Math.floor(Math.random() * 1e3 + 1)
-  let url = `/ajax/uncledatoolsbyajax.php?gid=${ajax.gid}&uc=${ajax.uc}&img=${ajax.img}&lang=zh&floor=${floor}`
-  response = await http({
-    method: 'get',
-    url,
-    headers: { referer: siteUrl + id }
-  })
-  $ = cheerio.load(response.data, {
-    xmlMode: true,
-    decodeEntities: true,
-    normalizeWhitespace: true
-  })
-  let $tr = $('tr')
-  if ($tr.length > 0) {
-    for (let i = 0; i < $tr.length; i++) {
-      let $a = $tr.eq(i).find('td:nth-child(2) a')
-      let $a1 = $tr.eq(i).find('td:nth-child(3) a')
-      let $subtitle = $tr.eq(i).find('a.btn-warning')
-      let $hd = $tr.eq(i).find('a.btn-primary')
-      if ($a.length === 0) continue
-      result.magnet.push({
-        link: decodeURI($a.attr('href').trim()),
-        size: $a.text().trim(),
-        dateTime: $a1.text().trim(),
-        is_hd: $hd.text().trim(),
-        has_subtitle: $subtitle.text().trim()
-      })
-    }
-  }
-
-  //   response = await httpV.get('/?s=' + id)
-  //   $ = cheerio.load(response.data, {
-  //     xmlMode: true,
-  //     decodeEntities: true,
-  //     normalizeWhitespace: true
-  //   })
-  //   let $div = $('div.entry-featured-media')
-  //   if ($div.length > 0) {
-  //     for (let i = 0; i < $div.length; i++) {
-  //       let $a2 = $div.eq(i).find('a.g1-frame')
-  //       if ($a2.length === 0) continue
-  //       const link = decodeURI($a2.attr('href').trim())
-  //       result.list.push(link)
-  //     }
-  //   }
-
-  response = await httpE.get('/video/' + id)
-  $ = cheerio.load(response.data, {
-    xmlMode: true,
-    decodeEntities: true,
-    normalizeWhitespace: true
-  })
-  let $div = $('div.thumb.c')
-  if ($div.length > 0) {
-    let list = []
-    for (let i = 0; i < $div.length; i++) {
-      let $a3 = $div.eq(i).find('a')
-      let $t3 = $div.eq(i).find('span.title')
-      let $d3 = $div.eq(i).find('span.duration')
-      let $i3 = $div.eq(i).find('img')
-      let $v3 = $div.eq(i).find('span.view')
-      $v3.find(':nth-child(n)').remove()
-      if ($a3.length === 0) continue
-      list.push({
-        title: $t3.text().trim(),
-        duration: $d3.text().trim(),
-        view: $v3.text().trim(),
-        cover: decodeURI($i3.attr('src').trim()),
-        link: embedyUrl + decodeURI($a3.attr('href').trim())
-      })
-    }
-    result.list = list.splice(0, 5)
-  }
-
-  return result
-}
 
 bot.onText(/\/xv (.+)/, async (msg, match) => {
   const today = moment().format('YYYY-MM-DD')
@@ -297,8 +148,8 @@ bot.onText(/\/xv (.+)/, async (msg, match) => {
   let chartType = msg.chat.type
   let isPrivate = chartType === 'private'
   let id = match[1].trim()
-  console.log('请求黄片', id)
-  if (isPrivate) bot.sendMessage(chatId, `开始查找黄片：${id} ……`)
+  console.log('开始请求', id)
+  if (isPrivate) bot.sendMessage(chatId, `开始关键字：${id} ……`)
   try {
     let result = await parseXtml(id)
     let max = isPrivate ? 10 : 3
@@ -329,14 +180,126 @@ bot.onText(/\/xv (.+)/, async (msg, match) => {
   } catch (e) {
     console.error(id, e.message)
     if (e.message.indexOf('timeout') !== -1)
-      return bot.sendMessage(chatId, '机器人查询黄片超时，请重试')
+      return bot.sendMessage(chatId, '机器人查询关键字超时，请重试')
     bot.sendMessage(chatId, `找不到 ${id}！`)
   }
 })
 
+bot.onText(/\/xm (.+)/, async (msg, match) => {
+  const today = moment().format('YYYY-MM-DD')
+  if (state.date[today]) state.date[today]++
+  else state.date[today] = 1
+  const chatId = msg.chat.id
+  let chartType = msg.chat.type
+  let id = match[1].trim()
+  console.log('开始请求', id)
+  let isPrivate = chartType === 'private'
+  if (isPrivate) bot.sendMessage(chatId, `开始推荐资源 ……`)
+  try {
+    let result = await parseBtml(id)
+    let max = isPrivate ? 10 : 3
+    if (result.list.length > 0) {
+      let message = result.title
+      result.list.every((list, i) => {
+        message +=
+          '\n----------------------\n点击观看: <a href="' +
+          list.link +
+          '">' +
+          list.title +
+          '</a>'
+        message += '\n时长：' + list.duration
+        return i + 1 < max
+      })
+      if (!isPrivate && result.list.length > max) {
+        message += `\n----------------------\n在群聊中发车，还有 ${result.magnet
+          .length -
+          max} 个Magnet链接没有显示\n与 ${robotName} 机器人单聊可以显示所有链接`
+      }
+      bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
+    } else {
+      bot.sendMessage(chatId, title + '还没有视频链接')
+    }
+  } catch (e) {
+    console.error(e.message)
+    if (e.message.indexOf('timeout') !== -1)
+      return bot.sendMessage(chatId, '机器人推荐资源超时，请重试')
+    bot.sendMessage(chatId, `找不到 ！`)
+  }
+})
+
+///////////////// 解析 ///////////////////////////////
+
+async function parseHtml(id) {
+  const result = { title: '', cover: '', magnet: [], list: [] }
+  let response = await httpGet({baseURL:javUrl,url:'/' + id})
+  let $ = cheerio.load(response.data)
+  let $image = $('a.bigImage img')
+  result.cover = javUrl + $image.attr('src')
+  result.title = $image.attr('title')
+  let ajax = { gid: '', uc: '', img: '' }
+  const context = new vm.createContext(ajax)
+  let $script = $('body > script:nth-child(9)')
+  new vm.Script($script.html()).runInContext(context)
+  let floor = Math.floor(Math.random() * 1e3 + 1)
+  let url = `/ajax/uncledatoolsbyajax.php?gid=${ajax.gid}&uc=${ajax.uc}&img=${ajax.img}&lang=zh&floor=${floor}`
+  response = await httpGet({baseURL:javUrl,url,headers: { referer: javUrl + id }})
+  $ = cheerio.load(response.data, {
+    xmlMode: true,
+    decodeEntities: true,
+    normalizeWhitespace: true
+  })
+  let $tr = $('tr')
+  if ($tr.length > 0) {
+    for (let i = 0; i < $tr.length; i++) {
+      let $a = $tr.eq(i).find('td:nth-child(2) a')
+      let $a1 = $tr.eq(i).find('td:nth-child(3) a')
+      let $subtitle = $tr.eq(i).find('a.btn-warning')
+      let $hd = $tr.eq(i).find('a.btn-primary')
+      if ($a.length === 0) continue
+      result.magnet.push({
+        link: decodeURI($a.attr('href').trim()),
+        size: $a.text().trim(),
+        dateTime: $a1.text().trim(),
+        is_hd: $hd.text().trim(),
+        has_subtitle: $subtitle.text().trim()
+      })
+    }
+  }
+
+  response = await httpGet({baseURL:embedyUrl,url:'/video/' + id})
+  $ = cheerio.load(response.data, {
+    xmlMode: true,
+    decodeEntities: true,
+    normalizeWhitespace: true
+  })
+  let $div = $('div.thumb.c')
+  if ($div.length > 0) {
+    let list = []
+    for (let i = 0; i < $div.length; i++) {
+      let $a3 = $div.eq(i).find('a')
+      let $t3 = $div.eq(i).find('span.title')
+      let $d3 = $div.eq(i).find('span.duration')
+      let $i3 = $div.eq(i).find('img')
+      let $v3 = $div.eq(i).find('span.view')
+      $v3.find(':nth-child(n)').remove()
+      if ($a3.length === 0) continue
+      list.push({
+        title: $t3.text().trim(),
+        duration: $d3.text().trim(),
+        view: $v3.text().trim(),
+        cover: decodeURI($i3.attr('src').trim()),
+        link: embedyUrl + decodeURI($a3.attr('href').trim())
+      })
+    }
+    result.list = list.splice(0, 5)
+  }
+
+  return result
+}
+
 async function parseXtml(id) {
   const result = { title: '', cover: '', magnet: [], list: [] }
-  let response = await httpX.get('/?k=' + encodeURI(id))
+  let response = await httpGet({baseURL:xvideoUrl,url:'/?k=' + encodeURI(id)})
   let $ = cheerio.load(response.data, {
     xmlMode: true,
     decodeEntities: true,
@@ -365,50 +328,9 @@ async function parseXtml(id) {
   return result
 }
 
-bot.onText(/\/xm (.+)/, async (msg, match) => {
-  const today = moment().format('YYYY-MM-DD')
-  if (state.date[today]) state.date[today]++
-  else state.date[today] = 1
-  const chatId = msg.chat.id
-  let chartType = msg.chat.type
-  let id = match[1].trim()
-  let isPrivate = chartType === 'private'
-  if (isPrivate) bot.sendMessage(chatId, `开始推荐 ……`)
-  try {
-    let result = await parseBtml(id)
-    let max = isPrivate ? 10 : 3
-    if (result.list.length > 0) {
-      let message = result.title
-      result.list.every((list, i) => {
-        message +=
-          '\n----------------------\n点击观看: <a href="' +
-          list.link +
-          '">' +
-          list.title +
-          '</a>'
-        message += '\n时长：' + list.duration
-        return i + 1 < max
-      })
-      if (!isPrivate && result.list.length > max) {
-        message += `\n----------------------\n在群聊中发车，还有 ${result.magnet
-          .length -
-          max} 个Magnet链接没有显示\n与 ${robotName} 机器人单聊可以显示所有链接`
-      }
-      bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
-    } else {
-      bot.sendMessage(chatId, title + '还没有视频链接')
-    }
-  } catch (e) {
-    console.error(e.message)
-    if (e.message.indexOf('timeout') !== -1)
-      return bot.sendMessage(chatId, '机器人查询黄片超时，请重试')
-    bot.sendMessage(chatId, `找不到 ！`)
-  }
-})
-
 async function parseBtml(type) {
   const result = { title: '', cover: '', magnet: [], list: [] }
-  let response = await httpB.get('/' + type)
+  let response = await httpGet({baseURL:xhamsterUrl,url:'/' + type})
   let $ = cheerio.load(response.data, {
     xmlMode: true,
     decodeEntities: true,
@@ -435,4 +357,27 @@ async function parseBtml(type) {
 
   // console.log('最终结果', result)
   return result
+}
+
+///////////////// 绘制 ///////////////////////////////
+function drawState(range) {
+  let now = moment()
+  let earlyDay = moment().subtract(range, 'day')
+  let date = [],
+    data = []
+  while (earlyDay.diff(now) <= 0) {
+    let dateKey = earlyDay.format('YYYY-MM-DD')
+    date.push(dateKey)
+    if (state.date[dateKey]) data.push(state.date[dateKey])
+    else data.push(0)
+    earlyDay = earlyDay.add(1, 'day')
+  }
+  let message =
+    '从 ' +
+    moment(state.start).fromNow() +
+    ' 开始工作\n\n       日期       : 查询车牌号次数'
+  date.forEach((d, i) => {
+    message += '\n' + d + ' : ' + data[i]
+  })
+  return message
 }
